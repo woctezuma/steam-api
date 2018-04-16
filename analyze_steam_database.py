4a45fcc374f9f1b28028f061f3f5a201643565d4
Wok <woctezuma@users.noreply.github.com>
2018-04-17 00:01:11 +0200

    Functions to load aggregated database and description keywords

--- analyze_steam_database.py
+++ analyze_steam_database.py
@@ -1,8 +1,30 @@
 from json_data_utils import load_data
 from steam_spy import get_steam_database_filename, get_steam_categories_filename, get_steam_genres_filename
 
+
+def load_aggregated_database():
+    steam_database = load_data(get_steam_database_filename())
+    all_categories = load_data(get_steam_categories_filename())
+    all_genres = load_data(get_steam_genres_filename())
+
+    return steam_database, all_categories, all_genres
+
+
+def get_description_keywords(steam_database, verbose=True):
+    description_keywords = set()
+    for appID in steam_database:
+        current_keywords = steam_database[appID].keys()
+        description_keywords = description_keywords.union(current_keywords)
+
+    description_keywords = sorted(description_keywords)
+
+    if verbose:
+        print('\n'.join(description_keywords))
+
+    return description_keywords
+
+
 if __name__ == '__main__':
-    print('Loading')
-    steamspy_database = load_data(get_steam_database_filename())
-    categories = load_data(get_steam_categories_filename())
-    genres = load_data(get_steam_genres_filename())
+    steamspy_database, categories, genres = load_aggregated_database()
+
+    keywords = get_description_keywords(steamspy_database)
