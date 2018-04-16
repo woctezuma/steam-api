160daa7ad3544856dc7016533b085c96cde55092
Wok <woctezuma@users.noreply.github.com>
2018-04-17 00:48:45 +0200

    Build a calendar of game releases, as a dict: datetime -> list of appIDs

--- analyze_steam_database.py
+++ analyze_steam_database.py
@@ -1,3 +1,5 @@
+import datetime
+
 from json_data_utils import load_data
 from steam_spy import get_steam_database_filename, get_steam_categories_filename, get_steam_genres_filename
 
@@ -10,7 +12,7 @@ def load_aggregated_database():
     return steam_database, all_categories, all_genres
 
 
-def get_description_keywords(steam_database, verbose=True):
+def get_description_keywords(steam_database, verbose=False):
     description_keywords = set()
     for appID in steam_database:
         current_keywords = steam_database[appID].keys()
@@ -19,12 +21,69 @@ def get_description_keywords(steam_database, verbose=True):
     description_keywords = sorted(description_keywords)
 
     if verbose:
+        print('\nDescription keywords:')
         print('\n'.join(description_keywords))
 
     return description_keywords
 
 
+def build_steam_calendar(steam_database, verbose=False):
+    # Objective: build a calendar of game releases, as a dict: datetime -> list of appIDs
+
+    release_calendar = dict()
+    weird_release_dates = set()
+    weird_counter = 0
+
+    for appID in steam_database:
+        release_date_as_str = steam_database[appID]['release_date']
+
+        if len(release_date_as_str) == 0:
+            continue
+
+        release_date_as_str = release_date_as_str.replace(',', '')  # "Nov 11, 2017" == "Nov 11 2017"
+        release_date_as_str = release_date_as_str.replace('st ', ' ')  # 1st
+        release_date_as_str = release_date_as_str.replace('nd ', ' ')  # 2nd
+        release_date_as_str = release_date_as_str.replace('rd ', ' ')  # 3rd
+        release_date_as_str = release_date_as_str.replace('th ', ' ')  # nth
+
+        try:
+            # Reference: https://stackoverflow.com/a/6557568/
+            release_date_as_datetime = datetime.datetime.strptime(release_date_as_str, '%b %d %Y')
+        except ValueError:
+            try:
+                release_date_as_datetime = datetime.datetime.strptime(release_date_as_str, '%d %b %Y')
+            except ValueError:
+                try:
+                    release_date_as_datetime = datetime.datetime.strptime(release_date_as_str, '%B %d %Y')
+                except ValueError:
+                    try:
+                        release_date_as_datetime = datetime.datetime.strptime(release_date_as_str, '%d %B %Y')
+                    except ValueError:
+                        weird_release_dates.add(release_date_as_str)
+                        weird_counter += 1
+                        if verbose:
+                            if weird_counter == 1:
+                                print('\nGames with weird release dates (wrong format, or missing the day):')
+                            print(steam_database[appID])
+                        continue
+
+        try:
+            release_calendar[release_date_as_datetime].append(appID)
+        except KeyError:
+            release_calendar[release_date_as_datetime] = [appID]
+
+    weird_release_dates = sorted(weird_release_dates)
+
+    if verbose:
+        print('\nWeird release dates:')
+        print('\n'.join(weird_release_dates))
+
+    return release_calendar, weird_release_dates
+
+
 if __name__ == '__main__':
     steamspy_database, categories, genres = load_aggregated_database()
 
-    keywords = get_description_keywords(steamspy_database)
+    keywords = get_description_keywords(steamspy_database, verbose=True)
+
+    steam_calendar, weird_dates = build_steam_calendar(steamspy_database, verbose=False)
