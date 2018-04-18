926f40b1e07541298a077f1840ce6e7b6cdb1980
Wok <woctezuma@users.noreply.github.com>
2018-04-18 19:35:08 +0200

    Draft to build map of tags, based on my steam-tag-mapping repository

---
+++ build_tag_map.py
@@ -0,0 +1,160 @@
+# Objective: create a map of Steam tags
+# Reference: https://github.com/woctezuma/steam-tag-mapping/blob/master/map_tags.py
+
+import numpy as np
+# Reference: https://stackoverflow.com/a/3054314
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from sklearn.decomposition import TruncatedSVD
+from sklearn.manifold import TSNE
+
+from analyze_steam_database import get_steam_database
+
+
+def preprocess_data(steam_database, categories_dict, genres_dict):
+    categories = list(categories_dict.values())
+    genres = list(genres_dict.values())
+
+    tags = categories
+    tags.extend(genres)
+
+    num_games = len(steam_database.keys())
+    print("#games = %d" % num_games)
+
+    num_tags = len(tags)
+    print("#tags = %d" % num_tags)
+
+    # Create a list of tags sorted in lexicographical order
+    tags_list = list(tags)
+    tags_list.sort()
+
+    tag_joint_game_matrix = np.zeros([num_tags, num_games])
+
+    game_counter = 0
+
+    for appid in steam_database:
+        categories = [categories_dict[str(i)] for i in steam_database[appid]['categories']]
+        genres = [genres_dict[str(i)] for i in steam_database[appid]['genres']]
+
+        current_tags = categories
+        current_tags.extend(genres)
+
+        for tag in current_tags:
+            i = tags_list.index(tag)
+            j = game_counter
+            tag_joint_game_matrix[i][j] += 1
+
+        game_counter += 1
+
+    return tag_joint_game_matrix, tags_list
+
+
+# Scale and visualize the embedding vectors
+# noinspection PyPep8Naming
+def plot_embedding(X, str_list, base_plot_filename=None, title=None, highlighted_tags=list(), delta_font=0.003):
+    # Code copied from: plot_embedding() in https://github.com/woctezuma/steam-tag-mapping/blob/master/map_tags.py
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    # noinspection PyPep8Naming
+    X = (X - x_min) / (x_max - x_min)
+
+    fig = Figure(dpi=300)
+    FigureCanvas(fig)
+    ax = fig.add_subplot(111)
+
+    ax.scatter(X[:, 0], X[:, 1])
+
+    # Add a label to each node. The challenge here is that we want to
+    # position the labels to avoid overlap with other labels
+    # References:
+    # * https://stackoverflow.com/a/40729950/
+    # * http://scikit-learn.org/stable/auto_examples/applications/plot_stock_market.html
+    for index, (label, x, y) in enumerate(
+            zip(str_list, X[:, 0], X[:, 1])):
+
+        dx = x - X[:, 0]
+        dx[index] = 1
+        dy = y - X[:, 1]
+        dy[index] = 1
+        this_dx = dx[np.argmin(np.abs(dy))]
+        this_dy = dy[np.argmin(np.abs(dx))]
+        if this_dx > 0:
+            horizontalalignment = 'left'
+            x = x + delta_font
+        else:
+            horizontalalignment = 'right'
+            x = x - delta_font
+        if this_dy > 0:
+            verticalalignment = 'bottom'
+            y = y + delta_font
+        else:
+            verticalalignment = 'top'
+            y = y - delta_font
+
+        if label in highlighted_tags:
+            my_color = "red"
+        else:
+            my_color = "black"
+
+        my_font_size = "xx-small"
+        my_weight = 'normal'
+        my_stretch = "condensed"
+
+        ax.text(x, y, label, color=my_color,
+                horizontalalignment=horizontalalignment,
+                verticalalignment=verticalalignment,
+                fontdict={'family': 'serif', 'weight': my_weight, 'size': my_font_size, 'stretch': my_stretch})
+
+    ax.set_xticks([]), ax.set_yticks([])
+    if title is not None:
+        ax.set_title(title)
+
+    if base_plot_filename is not None:
+        fig.savefig(base_plot_filename, bbox_inches='tight')
+
+    return
+
+
+def display_tag_map(embedding, tags_list, base_plot_filename=None, my_title=None,
+                    highlighted_tags=list()):
+    if len(highlighted_tags) == 0:
+        highlighted_tags = ['Early Access', 'Free to Play', 'In-App Purchases', 'Steam Trading Cards',
+                            'Violent', 'Gore']
+
+    plot_embedding(embedding, tags_list, base_plot_filename, my_title, highlighted_tags)
+
+    return
+
+
+def compute_tag_map(tag_joint_game_matrix):
+    # A first try, inspired from my steam-tag-mapping Github repository
+
+    num_components_svd = 50
+    num_components_tsne = 2
+
+    svd = TruncatedSVD(n_components=num_components_svd, random_state=0)
+
+    # We have chosen a learning rate lower than the default (1000) so that the error decreases during early iterations:
+    tsne = TSNE(n_components=num_components_tsne, random_state=0, verbose=2, learning_rate=400, perplexity=25)
+
+    # 1st step: reduce the dimensionality of the input SPARSE matrix, with TruncatedSVD as suggested in:
+    # http://scikit-learn.org/stable/modules/generated/sklearn.manifold.TSNE.html
+    reduced_matrix = svd.fit_transform(tag_joint_game_matrix)
+
+    # 2nd step: apply t-SNE to the reduced DENSE matrix
+    # noinspection PyPep8Naming
+    X = tsne.fit_transform(reduced_matrix)
+
+    return X
+
+
+if __name__ == '__main__':
+    steamspy_database, all_categories_dict, all_genres_dict = get_steam_database(verbosity=False)
+
+    joint_matrix, tags_list_sorted = preprocess_data(steamspy_database, all_categories_dict, all_genres_dict)
+
+    tag_embedding = compute_tag_map(joint_matrix)
+
+    plot_filename = 'tag_map.png'
+    plot_title = "Map of Steam tags"
+
+    display_tag_map(tag_embedding, tags_list_sorted, plot_filename, plot_title)
